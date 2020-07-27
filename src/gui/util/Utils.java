e853291b7136ad8aecc81772c638355b836c1beb
Alexandre Machado <machado_silva@hotmail.com>
2020-07-27 13:41:54 -0400

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -12,4 +12,13 @@ public class Utils {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
 	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
 }

a1e3befb5375064edf38b80182c318efd0525770
Alexandre Machado <machado_silva@hotmail.com>
2020-07-28 13:50:32 -0400

    TextField & DatePicker

--- src/gui/util/Constraints.java
+++ src/gui/util/Constraints.java
@@ -22,9 +22,14 @@ public class Constraints {
 
 	public static void setTextFieldDouble(TextField txt) {
 		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+				/*
 		    	if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
                     txt.setText(oldValue);
                 }
+		    	*/
+				if (newValue != null && !newValue.matches("\\d*([\\,]\\d*)?")) {
+					txt.setText(oldValue);
+				}
 		    });
 	}
 }

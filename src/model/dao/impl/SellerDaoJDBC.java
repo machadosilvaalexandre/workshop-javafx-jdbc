2c4d6947ce32b85a7a6fb364bb52b42cfe577e01
Alexandre Machado <machado_silva@hotmail.com>
2020-07-28 16:41:28 -0400

    Saving Seller

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,11 +34,8 @@ public class SellerDaoJDBC implements SellerDao {
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)");
-			/*
-					,
-					Statement.RETURN_GENERATED_KEYS);
-			*/
+					+ "(?, ?, ?, ?, ?)",Statement.RETURN_GENERATED_KEYS);
+			
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
